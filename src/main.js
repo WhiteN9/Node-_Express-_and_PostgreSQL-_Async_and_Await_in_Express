3b1fc1e0c6f2d42123c87a75f3b11f281a2ab6ec
White Nguyen <whitenguyen0909@gmail.com>
2022-06-07 22:05:18 -0600

    solution converted to async/await

--- src/main.js
+++ src/main.js
@@ -1,23 +1,36 @@
 const {
-    generateMessage,
-    goodbye,
-    generateSlogan,
-  } = require("../utils/slogan-generator");
-  
-  function getSlogan(request) {
-    generateSlogan(request).then(response => {
-      console.log(`Your request was: ${request}`);
-      console.log(`Your slogan is: ${response.slogan}`);
-    });
-  }
-  
-  function fullSession(request) {
-    generateMessage()
-      .then(console.log)
-      .then(() => getSlogan(request))
-      .then(() => goodbye())
-      .then(console.log);
-  }
-  
-  module.exports = { getSlogan, fullSession };
-  
+  generateMessage,
+  goodbye,
+  generateSlogan,
+} = require("../utils/slogan-generator");
+
+// function getSlogan(request) {
+//   generateSlogan(request).then((response) => {
+//     console.log(`Your request was: ${request}`);
+//     console.log(`Your slogan is: ${response.slogan}`);
+//   });
+// }
+
+async function getSlogan(request) {
+  const response = await generateSlogan(request);
+  console.log(`Your request was: ${request}`);
+  console.log(`Your slogan is: ${response.slogan}`);
+}
+
+async function fullSession(request) {
+  const greeting = await generateMessage();
+  console.log(greeting);
+  await getSlogan(request);
+  const leaving = await goodbye();
+  console.log(leaving);
+}
+
+// function fullSession(request) {
+//   generateMessage()
+//     .then(console.log)
+//     .then(() => getSlogan(request))
+//     .then(() => goodbye())
+//     .then(console.log);
+// }
+
+module.exports = { getSlogan, fullSession };
